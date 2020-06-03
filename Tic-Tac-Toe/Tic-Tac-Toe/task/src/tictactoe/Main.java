a6f62cdf7f513a8da0231077d7cb04d480be19f8
dmiwer <dmiwer@gmail.com>
2020-06-03 18:27:23 +0300

    Tic-Tac-Toe 1/5

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,10 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("XOX");
+        System.out.println("OXO");
+        System.out.println("XXO");
+    }
+}
