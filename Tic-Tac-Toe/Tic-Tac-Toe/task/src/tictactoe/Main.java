b57b78b1f394a561b9bb2fbc30506ced05145b6d
dmiwer <dmiwer@gmail.com>
2020-06-04 01:32:55 +0300

    Tic-Tac-Toe 4/5

--- Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,26 +1,55 @@
 package tictactoe;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        char[] arr = new Scanner(System.in).nextLine().toCharArray();
-        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        char[] arr = scanner.nextLine().toCharArray();
+        showField(arr);
+        while(true) {
+            System.out.print("Enter the coordinates: ");
+            String s = scanner.nextLine();
+            if(!Pattern.matches("^\\d+ \\d+$", s)) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+            String[] ars = s.split(" ");
+            int x = Integer.parseInt(ars[0]);
+            int y = Integer.parseInt(ars[1]);
+            if (x < 1 || x > 3 || y < 1 || y > 3){
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            int p = 8 + x - 3 * y;
+            if(arr[p] == 'X' || arr[p] == 'O'){
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+            arr[p] = 'X';
+            break;
+        }
+        showField(arr);
+//        System.out.println(verdict(arr));
+    }
+
+    private static void showField(char[] arr) {
         System.out.println("---------");
-        System.out.printf("| %c %c %c |%n", arr[i++], arr[i++], arr[i++]);
-        System.out.printf("| %c %c %c |%n", arr[i++], arr[i++], arr[i++]);
-        System.out.printf("| %c %c %c |%n", arr[i++], arr[i++], arr[i++]);
+        System.out.printf("| %c %c %c |%n", arr[0], arr[1], arr[2]);
+        System.out.printf("| %c %c %c |%n", arr[3], arr[4], arr[5]);
+        System.out.printf("| %c %c %c |%n", arr[6], arr[7], arr[8]);
         System.out.println("---------");
-        System.out.println(verdict(arr));
     }
 
     private static String verdict(char[] arr) {
         int countX = 0, countO = 0;
         int winX = 0, winO = 0;
-        for (char c : arr) {
-            if (c == 'X') countX++;
-            if (c == 'O') countO++;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 'X') countX++;
+            if(arr[i] == 'O') countO++;
         }
         if(arr[0] == arr[1] && arr[1] == arr[2] && arr[0] == 'X') winX++;
         if(arr[0] == arr[1] && arr[1] == arr[2] && arr[0] == 'O') winO++;
