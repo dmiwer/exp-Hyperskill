c68c9ad17ed01d7cd6f47a6ded36e8a7caff546f
dmiwer <dmiwer@gmail.com>
2020-06-13 04:14:24 +0300

    Numeral System Converter 3/6

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -6,6 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        System.out.println(num % 8);
+        int base = scanner.nextInt();
+        String s;
+        switch (base){
+            case (2):
+                s = "0b" + Integer.toBinaryString(num);
+                break;
+            case (8):
+                s = "0" + Integer.toOctalString(num);
+                break;
+            case (16):
+                s = "0x" + Integer.toHexString(num);
+                break;
+            default:
+                s = "xxx";
+        }
+        System.out.println(s);
     }
 }
