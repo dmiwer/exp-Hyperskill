f8134e33b83f0bf0c5475d7950bc1ca3e52ad4e4
dmiwer <dmiwer@gmail.com>
2020-06-14 11:00:54 +0300

    Numeral System Converter 6/6

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -4,40 +4,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static char[] arr = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    static char[] arr = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String ans;
 
-        int oldBase = scanner.nextInt();
-        scanner.skip("\\n");
-        String num = scanner.nextLine();
-        int newBase = scanner.nextInt();
+        try {
+            int oldBase = scanner.nextInt();
+            scanner.skip("\\n");
+            String num = scanner.nextLine();
+            int newBase = scanner.nextInt();
 
-        double n = fromOldBaseToDouble(num, oldBase);
-        if (newBase == 1) {
-            char[] c = new char[(int) n];
-            Arrays.fill(c, '1');
-            ans = new String(c);
-        } else ans = fromDoubleToNewBase(n, newBase);
+
+            double n = fromOldBaseToDouble(num, oldBase);
+            ans = fromDoubleToNewBase(n, newBase);
+        } catch (Exception e) {
+            System.out.println("error");
+            return;
+        }
 
         System.out.println(ans);
     }
 
     private static String fromDoubleToNewBase(double n, int newBase) {
+        if (newBase < 1 || newBase > 36) throw new IllegalArgumentException();
+        if (newBase == 1) {
+            char[] c = new char[(int) n];
+            Arrays.fill(c, '1');
+            return new String(c);
+        }
         int f = 1;
         while (f <= n) f *= newBase;
-        f /= newBase;
-        StringBuilder ans = new StringBuilder();
+        StringBuilder ans = new StringBuilder("");
         int intgr = (int) n;
         double fact = n - intgr;
-        while (f > 0) {
+        while ((f /= newBase) > 0) {
             ans.append(arr[intgr / f]);
             intgr %= f;
-            f /= newBase;
         }
-        if (ans.toString().equals("")) ans = new StringBuilder("0");
+        if (ans.length() == 0) ans.append("0");
         if (fact > 0) {
             ans.append('.');
             for (int i = 0; i < 5; i++) {
@@ -50,15 +56,18 @@ public class Main {
     }
 
     private static double fromOldBaseToDouble(String num, int oldBase) {
+        if (oldBase < 1 || oldBase > 36) throw new IllegalArgumentException();
+        if (oldBase == 1) return num.split("\\.")[0].length();
         long ans = 0;
         int f = -1;
+        int ch;
         for (char c : num.toCharArray()) {
             if (c == '.') {
-                if (oldBase == 1) break;
                 f = 0;
                 continue;
             }
-            ans = ans * oldBase + Arrays.binarySearch(arr, c);
+            if ((ch = Arrays.binarySearch(arr, c)) >= oldBase) throw new IllegalArgumentException();
+            ans = ans * oldBase + ch;
             f = f < 0 ? f : f + 1;
         }
         return f < 0 ? ans : ans / Math.pow(oldBase, f);
