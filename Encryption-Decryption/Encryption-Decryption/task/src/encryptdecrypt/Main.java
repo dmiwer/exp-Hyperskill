9475d5772237e44c86b6c161d1211931075d4f0e
dmiwer <dmiwer@gmail.com>
2020-06-16 02:27:55 +0300

    Encryption-Descryption 3/6

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation =scanner.nextLine();
         char[] str = scanner.nextLine().toCharArray();
         int shift  = scanner.nextInt();
-        for (int i = 0; i < str.length; i++)
-            if (str[i] > 96 && str[i] < 123)
-                str[i] = (char) ((str[i] - 97 + shift) % 26 + 97);
+
+        if (operation.equals("enc"))
+            encrypt(str, shift);
+        else if (operation.equals("dec"))
+            decrypt(str, shift);
 
         System.out.println(new String(str));
+    }
+
+    private static void encrypt(char[] str, int shift) {
+        for (int i = 0; i < str.length; i++)
+            str[i] = (char) ((str[i] + shift) % 256);
+    }
+
+    private static void decrypt(char[] str, int shift) {
+        for (int i = 0; i < str.length; i++)
+            str[i] = (char) ((256 + str[i] - shift) % 256);
     }
 }
