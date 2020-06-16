8cdcea8adc6f7194f9f5d59eadc7a24d30f0012a
dmiwer <dmiwer@gmail.com>
2020-06-16 18:57:07 +0300

    Encryption-Descryption 5/6

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,10 +1,17 @@
 package encryptdecrypt;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
         String mode = "enc";
         int shift = 0;
         char[] data = new char[0];
+        Path in = null;
+        Path out = null;
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case ("-mode"):
@@ -16,15 +23,35 @@ public class Main {
                 case ("-data"):
                     data = args[++i].toCharArray();
                     break;
+                case ("-in"):
+                    in = Paths.get(args[++i]);
+                    break;
+                case ("-out"):
+                    out = Paths.get(args[++i]);
             }
         }
 
+        if (data.length == 0 && in != null && Files.isRegularFile(in)) {
+            try {
+                data = Files.readString(in).toCharArray();
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+        }
         if (mode.equals("enc"))
             encrypt(data, shift);
         else if (mode.equals("dec"))
             decrypt(data, shift);
 
-        System.out.println(new String(data));
+
+        if (out != null) {
+            try {
+                Files.writeString(out, new String(data));
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+        } else
+            System.out.println(new String(data));
     }
 
     private static void encrypt(char[] str, int shift) {
