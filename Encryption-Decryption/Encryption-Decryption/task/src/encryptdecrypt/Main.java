8f289eef0682d3306d7b67146a1130c72f4ef628
dmiwer <dmiwer@gmail.com>
2020-06-26 20:35:44 +0300

    Encryption-Descryption 6/6

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,8 +5,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+interface Strategy {
+    void encrypt(char[] str, int shift);
+
+    void decrypt(char[] str, int shift);
+}
+
 public class Main {
     public static void main(String[] args) {
+        Strategy strategy = null;
         String mode = "enc";
         int shift = 0;
         char[] data = new char[0];
@@ -14,6 +21,10 @@ public class Main {
         Path out = null;
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
+                case ("-alg"):
+                    if (args[++i].equals("unicode"))
+                        strategy = new UnicodeStrategy();
+                    break;
                 case ("-mode"):
                     mode = args[++i];
                     break;
@@ -31,6 +42,9 @@ public class Main {
             }
         }
 
+        if (strategy == null)
+            strategy = new ShiftStrategy();
+
         if (data.length == 0 && in != null && Files.isRegularFile(in)) {
             try {
                 data = Files.readString(in).toCharArray();
@@ -39,9 +53,9 @@ public class Main {
             }
         }
         if (mode.equals("enc"))
-            encrypt(data, shift);
+            strategy.encrypt(data, shift);
         else if (mode.equals("dec"))
-            decrypt(data, shift);
+            strategy.decrypt(data, shift);
 
 
         if (out != null) {
@@ -53,13 +67,36 @@ public class Main {
         } else
             System.out.println(new String(data));
     }
+}
 
-    private static void encrypt(char[] str, int shift) {
+class ShiftStrategy implements Strategy {
+
+    @Override
+    public void encrypt(char[] str, int shift) {
+        for (int i = 0; i < str.length; i++){
+            if (str[i] >= 'A' && str[i] <= 'Z')
+                str[i] = (char) ((str[i] - 'A' + shift) % 26 + 'A');
+            else if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] = (char) ((str[i] - 'a' + shift) % 26 + 'a');
+        }
+    }
+
+    @Override
+    public void decrypt(char[] str, int shift) {
+        encrypt(str, 26 - shift);
+    }
+}
+
+class UnicodeStrategy implements Strategy {
+
+    @Override
+    public void encrypt(char[] str, int shift) {
         for (int i = 0; i < str.length; i++)
             str[i] = (char) ((str[i] + shift) % 256);
     }
 
-    private static void decrypt(char[] str, int shift) {
+    @Override
+    public void decrypt(char[] str, int shift) {
         for (int i = 0; i < str.length; i++)
             str[i] = (char) ((256 + str[i] - shift) % 256);
     }
